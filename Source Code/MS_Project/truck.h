1ff7add850eb3220580e9c7ee52905460e348a8f
Hyerin Mun <hmun@myseneca.ca>
2024-03-09 18:51:40 -0500

    Edited on the summary of functions

--- Source Code/MS_Project/truck.h
+++ Source Code/MS_Project/truck.h
@@ -43,19 +43,20 @@ int isValidInput(const struct Shipment* shipment);
 /// <returns>true (1) if route successfully found</returns>
 int getTruckRoute(struct Truck* truck, const struct Route* route);
 
-/// <summary>
-/// All our boxes are square, meaning that they have the same dimensions on all sides.
-/// This function assesses the validity of a given box size.
-/// The input parameter 'boxsize' represents the size of the box to be examined.
-/// Within the function, three constant values('1/2 (SIZE_SM)' '1 (SIZE_MED)' and '5 (SIZE_LG)') are defined to establish the acceptable box sizes.
-/// </summary>
-/// <param name="box_size">box size that will be loaded on the truck</param>
-/// <returns>true (1) if valid which means they are one of size among three constant values</returns>
-int isValidSize(double box_size);
+
 
 /// <summary>
-/// The trucks are also capable of carrying 50 cubic meters (MAX_VOL) of boxes.
+/// This function assesses the validity of a given box size that will be loaded on the truck. 
+/// The input parameter 'boxSize' represents the size of the box to be examined.
+/// Within the function, three constant values('1/2 (SIZE_SM)' '1 (SIZE_MED)' and '5 (SIZE_LG)') are defined to establish the acceptable box sizes.
+/// </summary>
+/// <param name="boxSize">box size that will be loaded on the truck</param>
+/// <returns>true (1) if valid which means they are one of size among three constant values</returns>
+int isValidSize(double boxSize);
+
+/// <summary>
 /// This function evaluates whether adding a box to a truck exceeds its volume capacity.
+/// The trucks are also capable of carrying 50 cubic meters (MAX_VOL) of boxes.
 /// </summary>
 /// <param name="truck"> truck on the route</param>
 /// <param name="box_size">box size that will be loaded on the truck</param>
@@ -63,8 +64,8 @@ int isValidSize(double box_size);
 int isValidVolume(struct Truck truck, double box_size );
 
 /// <summary>
-/// All our trucks are the same size and can hold up to 1200 kg (MAX_WEIGHT) of cargo
 /// This function assesses whether a truck becomes overloaded after incorporating a shipment.
+/// All our trucks are the same size and can hold up to 1200 kg (MAX_WEIGHT) of cargo
 /// If the current weight capacity of the truck is less than or equal to 1200, 
 /// it adds the weight of the shipment to the truck's capacity. 
 /// After the addition check if the updated capacity remains less than or equal to 1200. 
@@ -73,6 +74,14 @@ int isValidVolume(struct Truck truck, double box_size );
 /// <param name="shipment">shipment with its weight</param>
 /// <returns>true (1) if valid which means the weight is more than 0 and less than equal to 1200</returns>
 int isValidWeight(struct Truck truck, struct Shipment shipment);
+
+/// <summary>
+/// This function checks if the valid path is intersecting with building
+/// </summary>
+/// <param name="route"> valid truck route</param>
+/// <param name="map"> map structure </param>
+/// <returns></returns>
+int isvalidLocation(const struct Route route, const struct Map* map);
 #endif // !TRUCK_H
 
 
