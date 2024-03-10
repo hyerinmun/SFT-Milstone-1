8b9d22c09cd049e22bc298faec553cb8ff6587bc
Sang Yu Lee <slee567@myseneca.ca>
2024-03-09 20:01:15 -0500

    add definitions of all functions which have added in truck.h file into truck.c file

--- Source Code/MS_Project/truck.c
+++ Source Code/MS_Project/truck.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 #include "truck.h"
 
-int isValidInput(const struct Shipment* shipment)
+int getInput(const struct Shipment* shipment)
 {
 	return 0;
 }
@@ -12,4 +12,32 @@ int getTruckRoute(struct Truck* truck, const struct Route* route)
 	return 0;
 }
 
+int isValidSize(const double* boxSize)
+{
+	return 0;
+}
 
+int isValidVolume(const struct Truck* truck, const double* volume)
+{
+	return 0;
+}
+
+int isValidWeight(const double* weight)
+{
+	return 0;
+}
+
+int isTruckLoadable(const struct Truck* truck2Check, const struct Shipment* shipment)
+{
+	return 0;
+}
+
+int isvalidLocation(const struct Route* route, const struct Map* map)
+{
+	return 0;
+}
+
+int findClosestTruck(const struct Truck* truck, const struct Point* bldg)
+{
+	return 0;
+}
